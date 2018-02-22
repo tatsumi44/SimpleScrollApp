8b06969f6b3c23ba0baaf49b2d4a3f2cfb97bf03
tatsumi kentaro <tatsuken4290@gmail.com>
2018-02-22 22:17:55 +0900

    スワイプで画面遷移するサンプル

---
+++ Swift4PageViewApp/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  Swift4PageViewApp
+//
+//  Created by tatsumi kentaro on 2018/02/22.
+//  Copyright © 2018年 tatsumi kentaro. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
